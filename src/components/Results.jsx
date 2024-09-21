d16b7bbd4c5c0dd242018aa2c7283147e25cdf41
Shubiour <sv95.shuvo@gmail.com>
2024-09-21 21:42:26 +0600

    Lifting sate up from userInput to App component

---
+++ src/components/Results.jsx
@@ -0,0 +1,8 @@
+export default function Results({userInput}){
+  console.log(userInput)
+  return (
+    <p>
+      Results will be displayed here.
+    </p>
+  );
+}
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,21 +1,5 @@
-import { useState } from "react";
+export default function UserInput({handleChange, userInput}) {
 
-export default function UserInput() {
-  const [userInput, setUserInput] = useState({
-    initialInvestment: 10000,
-    annualInvestment: 1200,
-    expectedReturn: 6,
-    duration: 10
-  });
-
-  function handleChange(inputIdentifier, newValue) {
-    setUserInput(prevUserInput => {
-      return {
-        ...prevUserInput,
-        [inputIdentifier]: newValue,
-      }
-    });
-  }
 
   return (
     <section id="user-input">
